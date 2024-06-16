4582ef0b6944780c46691875a4efcfa70ac514a1
szudev <sz.ubiergo@gmail.com>
2024-06-15 23:34:19 -0400

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -55,8 +55,8 @@ function App() {
   };
 
   //const disableInputCondition = currentState === FILE_UPLOAD_STATES.UPLOADING;
-  const disableSubmitCondition =
-    currentState === (FILE_UPLOAD_STATES.IDLE || FILE_UPLOAD_STATES.UPLOADING);
+  //const disableSubmitCondition =
+  currentState === (FILE_UPLOAD_STATES.IDLE || FILE_UPLOAD_STATES.UPLOADING);
 
   const BUTTOM_TEXT = {
     [FILE_UPLOAD_STATES.IDLE]: "Without file",
@@ -89,7 +89,6 @@ function App() {
                 id="button"
                 type="submit"
                 name="Upload"
-                disabled={disableSubmitCondition}
                 className="flex items-center disabled:pointer-events-none justify-center gap-2 disabled:cursor-not-allowed hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800"
               >
                 {BUTTOM_TEXT[currentState]}{" "}
