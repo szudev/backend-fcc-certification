a066d35fc04b210adab443c9f86a4fed912f7956
szudev <sz.ubiergo@gmail.com>
2024-06-15 21:18:13 -0400

    fix input name to upfile instead of file

--- backend/src/routes/fileMetadataRoutes.ts
+++ backend/src/routes/fileMetadataRoutes.ts
@@ -7,6 +7,6 @@ const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-router.post("/fileanalyse", upload.single("file"), fileAnalyze);
+router.post("/fileanalyse", upload.single("upfile"), fileAnalyze);
 
 export default router;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -81,7 +81,7 @@ function App() {
               <input
                 className="block w-full text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                 type="file"
-                name="upload"
+                name="upfile"
                 onChange={handleOnChange}
                 disabled={disableInputCondition}
               />
